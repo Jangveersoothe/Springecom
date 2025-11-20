f56aebfca0ac85eeade7b7869a3502860ff0e86b
Jango <jangveersingh20@gmail.com>
2025-11-20 17:15:36 +0530

    first commit

--- src/main/java/com/ecom/productcatalog/controller/CategoryController.java
+++ src/main/java/com/ecom/productcatalog/controller/CategoryController.java
@@ -3,8 +3,9 @@ package com.ecom.productcatalog.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//just checking the commit
 @RestController
 @RequestMapping("/api/categories")
 public class CategoryController {
-    
+
 }
